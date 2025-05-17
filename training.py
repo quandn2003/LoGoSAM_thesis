a2891eb6ae418dfb0bec1b3d0cf9b4d2db6b24b8
Nguyen Dinh Quan <quannd@imt-soft.com>
2025-05-18 04:50:59 +0700

    add save model at the end

--- training.py
+++ training.py
@@ -166,7 +166,7 @@ def main(_run, _config, _log):
 
     _log.info('###### Training ######')
     epoch_losses = []
-    for sub_epoch in range(50):
+    for sub_epoch in range(1):
         print(f"Epoch: {sub_epoch}")
         _log.info(
             f'###### This is epoch {sub_epoch} of {n_sub_epoches} epoches ######')
